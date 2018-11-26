31cc61099ac77ef4636a81d0f67a1e482bba190a
Nick J. Horan <nick.j.horan@gmail.com>
2018-11-26 07:56:17 +0000

    Wiring up the React app to the Redux store

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
 	<div>
-		This is from my dashboard component
+		<ExpenseList />
 	</div>
 );
 

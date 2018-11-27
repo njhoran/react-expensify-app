baa8d93bbc4c9511ba175a7f7d64980ebbcedf15
Nick J. Horan <nick.j.horan@gmail.com>
2018-11-27 08:49:33 +0000

    Filtering the expenses list

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,17 @@
 'use strict';
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
 	<div>
 		<h3>{description}</h3>
 		<p>{amount} - {createdAt}</p>
+		<button onClick={(e) => {
+			dispatch(removeExpense({ id }));
+		}}>Remove</button>
 	</div>
 );
+
+export default connect()(ExpenseListItem);
